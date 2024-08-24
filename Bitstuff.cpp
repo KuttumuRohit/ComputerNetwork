e44bc69e3bd5de359e0478ec38c3d0145ce5e1a3
KuttumuRohit <rohitkuttum321@gmail.com>
2024-08-25 00:48:49 +0530

    completed the main body, but the function is still incomplete

--- Bitstuff.cpp
+++ Bitstuff.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
-int main(){
+void bitStuffing(int size,int array[]){
     
+}
+int main(){
+    int array[10]={1,1,1,1,1,0,1,1,1,1};
+    bitStuffing(10,array);
 }
